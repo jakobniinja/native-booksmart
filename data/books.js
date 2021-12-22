4cb3c5dedee1002391832c86bdbfd27bc5cc363d
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-22 15:34:25 +0100

    imported some data

---
+++ data/books.js
@@ -0,0 +1,32 @@
+export const books = () => {
+   return [ 
+    {
+
+        id: 0,
+        title: "",
+        image: "",
+        page: 0,
+        points: 0,
+        lastUpdateAt:"2021-02-05", 
+    },
+    
+    {
+
+        id: 1,
+        title: "",
+        image: "",
+        page: 0,
+        points: 0,
+        lastUpdateAt:"2021-02-05", 
+    },{
+
+        id: 2,
+        title: "",
+        image: "",
+        page: 0,
+        points: 0,
+        lastUpdateAt:"2021-02-05", 
+    },
+
+]
+}
