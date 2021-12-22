a559d90b30fecd6ac7f296373fcd5dccb8898c18
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-22 15:35:17 +0100

    Home wip- check expo app

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import ListItems from '../components/ListItems'
+
+export default function Home() {
+    return (
+        <View>
+            <ListItems  />
+        </View>
+    )
+}
