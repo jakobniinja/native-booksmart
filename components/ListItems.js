28c48f2e743ec4b065e917b7141d2a711b6e76e3
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-25 00:04:37 +0100

    tidy work

--- components/ListItems.js
+++ components/ListItems.js
@@ -2,21 +2,18 @@ import { ListItem, Avatar } from "react-native-elements";
 
 import React from "react";
 import { View, Text } from "react-native";
+import lightBulb from "../assets/lightBulb.png"
+import addBook from "../assets/addBook.png"
+import searchIcon from "../assets/searchIcon.png"
+
 
 export default function ListItems() {
   return (
     <View>
       <ListItem
       style={{marginTop: "7%"}}
-        friction={90} //
-        activeScale={0.95} //
-        linearGradientProps={{
-          colors: ["#FF9800", "#F44336"],
-          start: { x: 1, y: 0 },
-          end: { x: 0.2, y: 0 },
-        }}
       >
-        <Avatar rounded source={{ uri:  "https://o.remove.bg/downloads/b21ce967-a137-441e-b2ca-c78d9284ac31/add-book-icon-add-book-icon-115533862144zvpqmitfr-removebg-preview.png" }}  style={{width: "80px", height: "80px"}}  />
+        <Avatar rounded source={addBook}  style={{width: "80px", height: "80px"}}  />
         <ListItem.Content>
           <ListItem.Title style={{  fontWeight: "bold" }}>
             Books
@@ -29,18 +26,8 @@ export default function ListItems() {
       </ListItem>
       <ListItem
       style={{marginTop: "7%"}}
-        // Component={TouchableScale}
-        friction={90} //
-        tension={100} // These props are passed to the parent component (here TouchableScale)
-        activeScale={0.95} //
-        linearGradientProps={{
-          colors: ["#FF9800", "#F44336"],
-          start: { x: 1, y: 0 },
-          end: { x: 0.2, y: 0 },
-        }}
-        // ViewComponent={LinearGradient} // Only if no expo
       >
-        <Avatar rounded source={{ uri:  "https://github.com/jakobniinja/jakeHacker99-Book-Skimmer/blob/main/src/Assets/lookup.png?raw=true" }}  style={{width: "80px", height: "80px"}}  />
+        <Avatar rounded source={searchIcon}  style={{width: "80px", height: "80px"}}  />
         <ListItem.Content>
           <ListItem.Title style={{  fontWeight: "bold" }}>
             Dictionary
@@ -53,18 +40,8 @@ export default function ListItems() {
       </ListItem>
       <ListItem
       style={{marginTop: "7%"}}
-        // Component={TouchableScale}
-        friction={90} //
-        tension={100} // These props are passed to the parent component (here TouchableScale)
-        activeScale={0.95} //
-        linearGradientProps={{
-          colors: ["#FF9800", "#F44336"],
-          start: { x: 1, y: 0 },
-          end: { x: 0.2, y: 0 },
-        }}
-        // ViewComponent={LinearGradient} // Only if no expo
       >
-        <Avatar rounded source={{ uri:  "https://o.remove.bg/downloads/09d7b690-2eb8-4cdc-bc36-1ad44905be0b/e1032523533081.563248d9c852b-removebg-preview.png" }}  style={{width: "80px", height: "80px"}}  />
+        <Avatar rounded source={lightBulb }  style={{width: "80px", height: "80px"}}  />
         <ListItem.Content>
           <ListItem.Title style={{  fontWeight: "bold" }}>
             Vocabulary
