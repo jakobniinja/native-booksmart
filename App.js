79933827a260a3cd1a2fb65e1b9175e9cadeae2e
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-25 00:04:58 +0100

    darkMode wip

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import MyStack from './MyStack';
-
+import { View } from "react-native";
 
 export default function App() {
   return (
       <MyStack  />
   );
 }
-
--- MyStack.js
+++ MyStack.js
@@ -1,14 +1,29 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './screens/Home';
-import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
 const Stack = createStackNavigator();
+import { useColorScheme } from "react-native";
 
 export default function MyStack() {
+  const scheme = useColorScheme();
+
+  const MyTheme = {
+    ...DarkTheme,
+    colors: {
+      primary: "rgb(255, 45, 85)",
+      // background: "rgb(138,32,226)",
+      text: "rgb(28, 28, 30)",
+    },
+  };
   return (
-      <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Hem" component={Home} />
-    </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer theme={ scheme === 'dark' ? DarkTheme : DefaultTheme }>
+      <Stack.Navigator  >
+        <Stack.Screen  name="Hem" component={Home}   options={{headerTitleAlign: 'center',title: 'Hem'}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
