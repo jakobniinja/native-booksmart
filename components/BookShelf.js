39e0034a5b4f54217ce8ec3ce8af246c6137fb71
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-26 20:48:14 +0100

    impl grid for BookShelf

--- components/BookShelf.js
+++ components/BookShelf.js
@@ -1,60 +1,81 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState} from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { FlatGrid } from 'react-native-super-grid';
+import { Button } from "react-native-elements";
+import { useNavigation } from '@react-navigation/native';
 
-
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
+const styles = StyleSheet.create({
+  gridView: {
+    marginTop: 10,
+    flex: 1,
   },
-  paper: {
-    height: 100,
-    width: 100,
-    marginBottom: "20px"
+  itemContainer: {
+    justifyContent: 'flex-end',
+    borderRadius: 5,
+    padding: 10,
+    height: 150,
   },
-  text: {
-      display: "flex",
-      flexDirection: 'column',
-      height: 60,
-      width: 100,
-
+  itemName: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '600',
   },
-  textStyle:{
-        fontSize: 9,
-        fontWeight: 300
+  itemCode: {
+    fontWeight: '600',
+    fontSize: 12,
+    color: '#fff',
   },
-  control: {
-    padding: theme.spacing(2),
-  },
-}));
+});
 
 
 export default function BookShelf() {
-  const classes = useStyles();
+  const navigation = useNavigation();
+  const [items, setItems] = React.useState([
+    { name: 'TURQUOISE', code: '#1abc9c' },
+    { name: 'EMERALD', code: '#2ecc71' },
+    { name: 'PETER RIVER', code: '#3498db' },
+    { name: 'AMETHYST', code: '#9b59b6' },
+    { name: 'WET ASPHALT', code: '#34495e' },
+    { name: 'GREEN SEA', code: '#16a085' },
+    { name: 'NEPHRITIS', code: '#27ae60' },
+    { name: 'BELIZE HOLE', code: '#2980b9' },
+    { name: 'WISTERIA', code: '#8e44ad' },
+    { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
+    { name: 'SUN FLOWER', code: '#f1c40f' },
+    { name: 'CARROT', code: '#e67e22' },
+    { name: 'ALIZARIN', code: '#e74c3c' },
+    { name: 'CLOUDS', code: '#ecf0f1' },
+    { name: 'CONCRETE', code: '#95a5a6' },
+    { name: 'ORANGE', code: '#f39c12' },
+    { name: 'PUMPKIN', code: '#d35400' },
+    { name: 'POMEGRANATE', code: '#c0392b' },
+    { name: 'SILVER', code: '#bdc3c7' },
+    { name: 'ASBESTOS', code: '#7f8c8d' },
+  ]);
 
-    return (
-        <View  style={{marginTop:"7%"}}>
-          <Button variant="outlined" title="secondary" style={{marginBottom: "7%"}}   >Extend Collection </Button>
-              <Grid container className={classes.root} spacing={3}>
-      <Grid item xs={12}>
-        <Grid container justifyContent="center" spacing={3}>
-          {[0, 1, 2].map((value) => (
-            <Grid key={value} item>
-              <Paper className={classes.paper} />
-              <Paper className={classes.text}  >
-                  <Text className={classes.textStyle} >points :14 </Text>
-                  <Text className={classes.textStyle} >pages : 320 </Text>
-                  <Text className={classes.textStyle} >lastRead : 02/17</Text>
-                   </Paper>
-            </Grid>
-          ))}
-        </Grid>
-      </Grid>
-    </Grid>
+  return (
+    <>
+         <Button title='Extend Collection'  onPress={() => navigation.navigate('AddBook') } 
+                titleStyle={{ fontWeight: '700' }}
+               buttonStyle={{
+                       backgroundColor: 'rgba(92, 99,216, 1)',
+                  borderColor: 'transparent',
+                  borderWidth: 0,
+                  borderRadius: 5,
+                  paddingVertical: 10,
+                }}/>
+    <FlatGrid
+      itemDimension={130}
+      data={items}
+      style={styles.gridView}
+      spacing={10}
+      renderItem={({ item }) => (
+        <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+          <Text style={styles.itemName}>{item.name}</Text>
+          <Text style={styles.itemCode}>{item.code}</Text>
         </View>
-    )
+      )}
+    />
+    </>
+  );
 }
