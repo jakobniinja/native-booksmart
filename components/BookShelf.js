8b41c2aac327da121842712a451e438a35c75ca5
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-27 18:02:19 +0100

    firebase crud op - wip

--- components/BookShelf.js
+++ components/BookShelf.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { FlatGrid } from 'react-native-super-grid';
+import React, { useContext, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { FlatGrid } from "react-native-super-grid";
 import { Button } from "react-native-elements";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import AppContext from "../Context/AppContext";
 
 const styles = StyleSheet.create({
   gridView: {
@@ -10,72 +12,86 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   itemContainer: {
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
     borderRadius: 5,
     padding: 10,
     height: 150,
   },
   itemName: {
     fontSize: 16,
-    color: '#fff',
-    fontWeight: '600',
+    color: "#fff",
+    fontWeight: "600",
   },
   itemCode: {
-    fontWeight: '600',
+    fontWeight: "600",
     fontSize: 12,
-    color: '#fff',
+    color: "#fff",
   },
 });
 
-
 export default function BookShelf() {
+  const { setBook2 } = useContext(AppContext);
   const navigation = useNavigation();
   const [items, setItems] = React.useState([
-    { name: 'TURQUOISE', code: '#1abc9c' },
-    { name: 'EMERALD', code: '#2ecc71' },
-    { name: 'PETER RIVER', code: '#3498db' },
-    { name: 'AMETHYST', code: '#9b59b6' },
-    { name: 'WET ASPHALT', code: '#34495e' },
-    { name: 'GREEN SEA', code: '#16a085' },
-    { name: 'NEPHRITIS', code: '#27ae60' },
-    { name: 'BELIZE HOLE', code: '#2980b9' },
-    { name: 'WISTERIA', code: '#8e44ad' },
-    { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
-    { name: 'SUN FLOWER', code: '#f1c40f' },
-    { name: 'CARROT', code: '#e67e22' },
-    { name: 'ALIZARIN', code: '#e74c3c' },
-    { name: 'CLOUDS', code: '#ecf0f1' },
-    { name: 'CONCRETE', code: '#95a5a6' },
-    { name: 'ORANGE', code: '#f39c12' },
-    { name: 'PUMPKIN', code: '#d35400' },
-    { name: 'POMEGRANATE', code: '#c0392b' },
-    { name: 'SILVER', code: '#bdc3c7' },
-    { name: 'ASBESTOS', code: '#7f8c8d' },
+    { name: "TURQUOISE", code: "#1abc9c" },
+    { name: "EMERALD", code: "#2ecc71" },
+    { name: "PETER RIVER", code: "#3498db" },
+    { name: "AMETHYST", code: "#9b59b6" },
+    { name: "WET ASPHALT", code: "#34495e" },
+    { name: "GREEN SEA", code: "#16a085" },
+    { name: "NEPHRITIS", code: "#27ae60" },
+    { name: "BELIZE HOLE", code: "#2980b9" },
+    { name: "WISTERIA", code: "#8e44ad" },
+    { name: "MIDNIGHT BLUE", code: "#2c3e50" },
+    { name: "SUN FLOWER", code: "#f1c40f" },
+    { name: "CARROT", code: "#e67e22" },
+    { name: "ALIZARIN", code: "#e74c3c" },
+    { name: "CLOUDS", code: "#ecf0f1" },
+    { name: "CONCRETE", code: "#95a5a6" },
+    { name: "ORANGE", code: "#f39c12" },
+    { name: "PUMPKIN", code: "#d35400" },
+    { name: "POMEGRANATE", code: "#c0392b" },
+    { name: "SILVER", code: "#bdc3c7" },
+    { name: "ASBESTOS", code: "#7f8c8d" },
   ]);
 
   return (
     <>
-         <Button title='Extend Collection'  onPress={() => navigation.navigate('AddBook') } 
-                titleStyle={{ fontWeight: '700' }}
-               buttonStyle={{
-                       backgroundColor: 'rgba(92, 99,216, 1)',
-                  borderColor: 'transparent',
-                  borderWidth: 0,
-                  borderRadius: 5,
-                  paddingVertical: 10,
-                }}/>
-    <FlatGrid
-      itemDimension={130}
-      data={items}
-      style={styles.gridView}
-      spacing={10}
-      renderItem={({ item }) => (
-        <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
-          <Text style={styles.itemName}>{item.name}</Text>
-          <Text style={styles.itemCode}>{item.code}</Text>
-        </View>
-      )}
-    />
+      <Button
+        title="Extend Collection"
+        onPress={(book) => {
+          navigation.navigate("AddBook");
+        }}
+        titleStyle={{ fontWeight: "700" }}
+        buttonStyle={{
+          backgroundColor: "#8a2be2",
+          borderColor: "transparent",
+          borderWidth: 0,
+          borderRadius: 5,
+          paddingVertical: 10,
+        }}
+      />
+      <FlatGrid
+        itemDimension={130}
+        data={items}
+        style={styles.gridView}
+        spacing={10}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => {
+              setBook2({ name: item.name,code: item.code });
+              navigation.navigate("UpdateBook");
+            }}
+          >
+            <View
+              style={[styles.itemContainer, { backgroundColor: item.code }]}
+            >
+              <Text style={styles.itemName}>{item.name}</Text>
+              <Text style={styles.itemCode}>{item.code}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     </>
   );
 }
