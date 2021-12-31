d5054b62ac9a6e4f5f4634037905a4751874a5f3
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-31 18:54:53 +0100

    impl textinput natively

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import ListItems from '../components/ListItems'
+import { useTheme } from '@react-navigation/native';
+
 
 export default function Home() {
+    const {colors} =  useTheme();
     return (
-        <View>
+        <View   >
             <ListItems  />
         </View>
     )
