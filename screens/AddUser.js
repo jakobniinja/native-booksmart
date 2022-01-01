f0871170b5070c8e10edf28c9b3fe871e5291cd0
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-01 18:38:37 +0100

    impl add user

---
+++ screens/AddUser.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, ScrollView } from "react-native";
+import { Button } from "react-native-elements";
+
+import firebase from "../Firebase";
+const AddUserScreen = (props) => {
+  const initalState = {
+    name: "",
+    age: "",
+    occupation: "",
+  };
+
+  const [state, setState] = useState(initalState);
+
+  const handleChangeText = (value, name) => {
+    setState({ ...state, [name]: value });
+  };
+
+  const saveNewUser = async () => {
+    if (state.name === "") {
+      alert("please provide a name");
+    } else {
+      try {
+        await firebase.db.collection("users").add({
+          name: state.name,
+          age: state.age,
+          occupation: state.occupation,
+        });
+
+        props.navigation.navigate("UsersList");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      {/* Name Input */}
+      <View style={styles.inputGroup}>
+        <TextInput
+          placeholder="fullName"
+          onChangeText={(value) => handleChangeText(value, "name")}
+          value={state.name}
+        />
+      </View>
+
+      {/* age Input */}
+      <View style={styles.inputGroup}>
+        <TextInput
+          placeholder="Age"
+          multiline={true}
+          numberOfLines={4}
+          onChangeText={(value) => handleChangeText(value, "age")}
+          value={state.age}
+        />
+      </View>
+
+      {/* Input */}
+      <View style={styles.inputGroup}>
+        <TextInput
+          placeholder="Occupation"
+          onChangeText={(value) => handleChangeText(value, "occupation")}
+          value={state.occupation}
+        />
+      </View>
+
+      <View style={styles.button}>
+        <Button
+          title="Save User"
+          onPress={() => saveNewUser()}
+          buttonStyle={{
+            backgroundColor: "gray",
+          }}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35,
+  },
+  inputGroup: {
+    flex: 1,
+    padding: 0,
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  loader: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default AddUserScreen;
