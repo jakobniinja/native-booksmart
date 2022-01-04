f6538c9f192567e4efc7cdfcf1f708633e44715c
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-04 22:41:34 +0100

    fixed bracket issue

--- components/BookGrid.js
+++ components/BookGrid.js
@@ -53,86 +53,63 @@ export default function BookGrid(props) {
   }, [currentUser]);
 
   console.log(currentUser.id);
-  // books.forEach((book) => console.log(`${currentUser.name} has these books  ${book.title}`))
-  console.log(allBooks.forEach((book, index) => {
-    console.log("book", book.title, " index:", index)
-    console.log(allBooks.length)
-  }))
   return (
     <SortableGridview
-      data={[
-        { name: "book 1", backgroundColor: "#09f", color: "#fff" },
-        {
-          name: "book 2",
-          backgroundColor: "#rgba(255, 216, 58, 1)",
-          color: "#333",
-        },
-        {
-          name: "book 3",
-          backgroundColor: "#rgba(0, 222, 144, 1)",
-          color: "#fff",
-        }
-      ]}
-      lockData={books.forEach((book) => {
-        [{ name: `${book.title}`, backgroundColor: "#09f", color: "#fff" }];
-      })}
-      onDragStart={() => {
-        console.log("LockItemCoverLayout onDragStart");
-      }}
-      onDragRelease={(data) => {
-        console.log("LockItemCoverLayout onDragRelease", data);
-      }}
-      renderItem={(item, index) => {
-
-        return (
-          <View
-            uniqueKey={item.name}
-            style={[styles.item, { backgroundColor: item.backgroundColor }]}
-            onTap={() => {
-              Alert.alert(`On Tap ${item.name}!`);
-            }}
-          >
-            {/* <Text style={[styles.text, { color: item.color }]}>
-              {item.name}
-            </Text> */}
-            {allBooks.length >=1
-              ? (
-              allBooks.map((book, index) => (
-                <Text key={index} style={{color: "black"}} >
-              {index}
-              </Text>))
-              ) :(<Text style={{color: "gray"}} >
-
-                shu Bror
-              </Text>)}
-          </View>
-        );
-      }}
-      renderLockItem={(item, index) => {
-        return (
-          <View
-            uniqueKey={`${item.name}`}
-            style={styles.lock}
-            onTap={() => {
-              Alert.alert(`On Tap ${item.name}!`);
-            }}
-          >
-            <Text>{item.name}</Text>
-          </View>
-        );
-      }}
-      lockItemCoverStyle={{ marginTop: -8, marginLeft: -8 }}
-      renderLockItemCover={(item, index) => {
-        return (
-          <TouchableOpacity
-            style={styles.cover}
-            onPress={() => {
-              Alert.alert(`On Press ${item.name} Cover!`);
-            }}
-          ></TouchableOpacity>
-        );
-      }}
-    />
+  data={[
+    {name: `124`, backgroundColor: '#09f', color: '#fff'},
+    {name: 'book 2', backgroundColor: '#rgba(255, 216, 58, 1)', color: '#333'},
+    {name: 'book 3', backgroundColor: '#rgba(0, 222, 144, 1)', color: '#fff'},
+  ]}
+  lockData={[
+    {name: 'book 2 '},
+    {name: 'book 4 '},
+  ]}
+  onDragStart={() => {
+    console.log('LockItemCoverLayout onDragStart');
+  }}
+  onDragRelease={(data) => {
+    console.log('LockItemCoverLayout onDragRelease', data);
+  }}
+  renderItem={(item, index) => {
+    return (
+      <View
+        uniqueKey={item.name}
+        style={[styles.item, {backgroundColor: item.backgroundColor}]}
+        onTap={() => {
+          Alert.alert(`On Tap ${item.name}!`);
+        }}
+      >
+        <Text style={[styles.text, {color: item.color}]}>{item.name}</Text>
+      </View>
+    )
+  }}
+  renderLockItem={(item, index) => {
+    return (
+      <View
+        uniqueKey={`${item.name}`}
+        style={styles.lock}
+        onTap={() => {
+          Alert.alert(`On Tap ${item.name}!`);
+        }}
+      >
+        <Text>{item.name}</Text>
+      </View>
+    )
+  }}
+  lockItemCoverStyle={{marginTop: -8, marginLeft: -8
+}}
+  renderLockItemCover={(item, index) => {
+    return (
+      <TouchableOpacity
+        style={styles.cover}
+        onPress={() => {
+          Alert.alert(`On Press ${item.name} Cover!`);
+        }}
+      >
+      </TouchableOpacity>
+    )
+  }}
+/>
   );
 }
 
