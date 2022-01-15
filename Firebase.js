46f693c27d264317b07cef0072e3e0026c7a81d5
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-15 21:10:18 +0100

    reinit firebase

--- Firebase.js
+++ Firebase.js
@@ -1,6 +1,8 @@
-import firebase from "firebase";
-import "firebase/firestore";
-
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,11 +15,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-
-export default {
-  firebase,
-  db
-};
+const app = initializeApp(firebaseConfig);
+export const db =  getFirestore(app);
