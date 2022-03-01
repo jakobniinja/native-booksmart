4dd159b87cfd0308088e42950f8b9ca534c6ec89
jakobniinja <jakobyaro127@hotmail.com>
2022-03-01 22:10:39 +0100

    impl react-.native-dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,30 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    env:{
+    presets: ["babel-preset-expo"],
+
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: null,
+          blacklist: null, // DEPRECATED
+          whitelist: null, // DEPRECATED
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+    ],
+
+    env: {
       production: {
-        plugins: ['react-native-paper/babel']
-      }
-    }
+        plugins: ["react-native-paper/babel"],
+      },
+    },
   };
 };
