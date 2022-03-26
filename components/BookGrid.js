a6af7f708fac94f99c13a89a9b7c06c0707fd9c5
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-03-26 18:10:08 +0100

    added: olivas books

--- components/BookGrid.js
+++ components/BookGrid.js
@@ -8,191 +8,46 @@ import {
   Dimensions,
   ScrollView,
 } from "react-native";
-import { doc, collection, onSnapshot,  query,  getDocs } from "firebase/firestore";
-import SortableGridview from "react-native-sortable-gridview";
+import {
+  doc,
+  collection,
+  onSnapshot,
+  query,
+  getDocs,
+} from "firebase/firestore";
 import AppContext from "../Context/AppContext";
 import { db } from "../Firebase";
-import graphql from "../assets/books/graphql.png"
-import javaalgo from "../assets/books/javaalgo.jpg"
-import algo2 from "../assets/books/algo2.jpeg"
-import l1 from "../assets/books/lm1-rs.jpg"
-import l2 from "../assets/books/lm2-rs.jpg"
-
-
-
-import da1 from "../assets/books/dfmf-rs.jpg"
-import da2 from "../assets/books/dfmf2-rs.jpg"
-import da3 from "../assets/books/dfmf3-rs.jpg"
-
-
-
 
 import { initialWindowMetrics } from "react-native-safe-area-context";
+import BookGridHelper from "./BookGridHelper";
 export default function BookGrid(props) {
   const { user, setUser } = useContext(AppContext);
- const [flag, setFlag] = useState(false)
-   useMemo(
-    () => ({flag, user}),
-    [flag, user]
-  );
-
- 
-  const [allBooks, setAllBooks] = useState([]);
-  var bookie;
-const [bookLogic, setBookLogic] = useState(
-
-[
-        { name: `book1`, backgroundColor: "#09f", color: "#000"  },
-        {
-          name: "book2",
-          color: "#000",
-          backgroundColor: 'blueviolet'
-        },
-        {
-          name: "book3",
-          backgroundColor: "#rgba(0, 222, 144, 1)",
-          color: "#000",
-        },
-      
-])
-
-  let data2= [
-         { name: ` 124`, backgroundColor: "#09f", color: "#000", backgroundImage: `url(${da3})` },
-         {
-           name: "Java Algrothims ",
-           color: "#000",
-     backgroundImage:`url(${da1})`, 
-         },
-         {
-           name: "Graphl mm",
-           backgroundColor: "#rgba(0, 222, 144, 1)",
-           color: "#000",
-     backgroundImage:`url(${da2})`, 
-         },
-  ]
-
-
-
-//    {user.name == "Tofflan" && setData(
-//  [
-//          { name: ` 124`, backgroundColor: "#09f", color: "#000", backgroundImage: `url(${da3})` },
-//          {
-//            name: "Java Algrothims ",
-//            color: "#000",
-//      backgroundImage:`url(${da1})`, 
-//          },
-//          {
-//            name: "Graphl mm",
-//            backgroundColor: "#rgba(0, 222, 144, 1)",
-//            color: "#000",
-//      backgroundImage:`url(${da2})`, 
-//          },
-//        ]
-//    )}
-
-   
-
+  const [flag, setFlag] = useState(false);
+  useMemo(() => ({ flag, user }), [flag, user]);
 
   useEffect(() => {
-      const userCol = collection(db, "users");
-          const getUser = async () => {
+    const userCol = collection(db, "users");
+    const getUser = async () => {
       const data = await getDocs(userCol);
     };
     const q = query(collection(db, "users"));
     const unsubscribe = onSnapshot(q, () => {
       getUser();
-      
     });
-
   }, []);
-  
-
-  
-
-  return (
-    user.name!="Tofflan" ? (
-
-    <SortableGridview
-      data={bookLogic}
-      onDragStart={() => {
-        console.log("LockItemCoverLayout onDragStart");
-      }}
-      onDragRelease={(data) => {
-        console.log("LockItemCoverLayout onDragRelease", data);
-      }}
-      renderItem={(item, index) => {
-        return (
-          <View
-            key={[item.name]}
-            style={[styles.item, {
-               backgroundColor: item.backgroundColor, width: '100px', height: '130px', resizeMode: 'cover',
-            backgroundImage: item.backgroundImage
-            }]}
-            onTap={() => {
-              Alert.alert(`On Tap ${item.name}!`);
-            }}
-          >
-            <Text style={[styles.text, { color: item.color }]} onPress={() => {
-                  setFlag(!flag)
-            }} >
-              {item.name}
-            </Text>
-
-          </View>
-        );
-      }}
-    />
-
-    ) : (
-
-    <SortableGridview
-      data={data2}
-      onDragStart={() => {
-        console.log("LockItemCoverLayout onDragStart");
-      }}
-      onDragRelease={(data) => {
-        console.log("LockItemCoverLayout onDragRelease", data);
-      }}
-      renderItem={(item, index) => {
-        return (
-          <View
-            key={[item.name]}
-            style={[styles.item, {
-               backgroundColor: item.backgroundColor, width: '100px', height: '130px', resizeMode: 'cover',
-            backgroundImage: item.backgroundImage
-            }]}
-            onTap={() => {
-              Alert.alert(`On Tap ${item.name}!`);
-            }}
-          >
-            <Text style={[styles.text, { color: item.color }]} onPress={() => {
-                  setFlag(!flag)
-            }} >
-              {item.name}
-            </Text>
-
-          </View>
-        );
-      }}
-    />
-
-    )
-
-    
-
-  )
+  return <BookGridHelper />;
 }
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 15,
-    backgroundColor: '#ddd',
+    backgroundColor: "#ddd",
   },
   item: {
     width: 80,
     borderRadius: 12,
     height: 70,
-    color: '#fff',
+    color: "#fff",
     alignItems: "center",
     justifyContent: "center",
     marginLeft: "16%",
---
+++ components/BookGridHelper.js
@@ -0,0 +1,289 @@
+import { View, Text, StyleSheet } from "react-native";
+import SortableGridview from "react-native-sortable-gridview";
+import React, { useContext, useState } from "react";
+import AppContext from "../Context/AppContext";
+import l1 from "../assets/books/lm1-rs.jpg";
+import l2 from "../assets/books/lm2-rs.jpg";
+
+import da1 from "../assets/books/dfmf-rs.jpg";
+import da2 from "../assets/books/dfmf2-rs.jpg";
+import da3 from "../assets/books/dfmf3-rs.jpg";
+import hp from "../assets/books/hp-full (1).jpg";
+import h1 from "../assets/books/filosofi-stenen-rs.jpg";
+import h2 from "../assets/books/halvblodsprinsen-rs.jpg";
+import h3 from "../assets/books/fenfix-rs.jpg";
+
+
+export default function BookGridHelper() {
+  const { user, setUser } = useContext(AppContext);
+  const [bookLogic, setBookLogic] = useState([
+    { name: `book1`, backgroundColor: "#09f", color: "#000" },
+    {
+      name: "book2",
+      color: "#000",
+      backgroundColor: "blueviolet",
+    },
+    {
+      name: "book3",
+      backgroundColor: "#rgba(0, 222, 144, 1)",
+      color: "#000",
+    },
+  ]);
+
+  let data2 = [
+    {
+      name: "På Ny Kula",
+      backgroundColor: "#09f",
+      color: "#000",
+      backgroundImage: `url(${da3})`,
+    },
+    {
+      name: "Bravader ",
+      color: "#000",
+      backgroundImage: `url(${da1})`,
+    },
+    {
+      name: "Nya Fältet",
+      backgroundColor: "#rgba(0, 222, 144, 1)",
+      color: "#000",
+      backgroundImage: `url(${da2})`,
+    },
+  ];
+  let data3 = [
+    {
+      name: "Hotell Mysteriet ",
+      color: "#000",
+      backgroundImage: `url(${l1})`,
+    },
+    {
+      name: "Diamant Myseriet",
+      backgroundColor: "#rgba(0, 222, 144, 1)",
+      color: "#000",
+      backgroundImage: `url(${l2})`,
+    },
+  ];
+
+  let data4 = [
+    {
+      name: "Halvblodsprinsen",
+      color: "#000",
+      backgroundImage: `url(${h1})`,
+    },
+    {
+      name: "Filosofi stenen",
+      backgroundColor: "#rgba(0, 222, 144, 1)",
+      color: "#000",
+      backgroundImage: `url(${h2})`,
+    },
+    {
+      name: "Fenixordern",
+      backgroundColor: "#rgba(0, 222, 144, 1)",
+      color: "#000",
+      backgroundImage: `url(${h3})`,
+    },
+
+  ];
+  if (user.name == "Tofflan") {
+    return (
+      <SortableGridview
+        data={data2}
+        onDragStart={() => {
+          console.log("LockItemCoverLayout onDragStart");
+        }}
+        onDragRelease={(data) => {
+          console.log("LockItemCoverLayout onDragRelease", data);
+        }}
+        renderItem={(item, index) => {
+          return (
+            <View
+              key={[item.name]}
+              style={[
+                styles.item,
+                {
+                  backgroundColor: item.backgroundColor,
+                  width: "100px",
+                  height: "130px",
+                  resizeMode: "cover",
+                  backgroundImage: item.backgroundImage,
+                },
+              ]}
+              onTap={() => {
+                Alert.alert(`On Tap ${item.name}!`);
+              }}
+            >
+              <Text
+                style={[styles.text, { color: item.color }]}
+                onPress={() => {
+                  setFlag(!flag);
+                }}
+              >
+                {item.name}
+              </Text>
+            </View>
+          );
+        }}
+      />
+    );
+  } else if (user.name == "Lilla Captain") {
+    return (
+      <SortableGridview
+        data={data3}
+        onDragStart={() => {
+          console.log("LockItemCoverLayout onDragStart");
+        }}
+        onDragRelease={(data) => {
+          console.log("LockItemCoverLayout onDragRelease", data);
+        }}
+        renderItem={(item, index) => {
+          return (
+            <View
+              key={[item.name]}
+              style={[
+                styles.item,
+                {
+                  backgroundColor: item.backgroundColor,
+                  width: "100px",
+                  height: "130px",
+                  resizeMode: "cover",
+                  backgroundImage: item.backgroundImage,
+                },
+              ]}
+              onTap={() => {
+                Alert.alert(`On Tap ${item.name}!`);
+              }}
+            >
+              <Text
+                style={[styles.text, { color: item.color }]}
+                onPress={() => {
+                  setFlag(!flag);
+                }}
+              >
+                {item.name}
+              </Text>
+            </View>
+          );
+        }}
+      />
+    );
+  } 
+   else if (user.name == "Olivia") {
+    return (
+      <SortableGridview
+        data={data4}
+        onDragStart={() => {
+          console.log("LockItemCoverLayout onDragStart");
+        }}
+        onDragRelease={(data) => {
+          console.log("LockItemCoverLayout onDragRelease", data);
+        }}
+        renderItem={(item, index) => {
+          return (
+            <View
+              key={[item.name]}
+              style={[
+                styles.item,
+                {
+                  backgroundColor: item.backgroundColor,
+                  width: "100px",
+                  height: "130px",
+                  resizeMode: "cover",
+                  backgroundImage: item.backgroundImage,
+                },
+              ]}
+              onTap={() => {
+                Alert.alert(`On Tap ${item.name}!`);
+              }}
+            >
+              <Text
+                style={[styles.text, { color: item.color }]}
+                onPress={() => {
+                  setFlag(!flag);
+                }}
+              >
+                {item.name}
+              </Text>
+            </View>
+          );
+        }}
+      />
+    );
+  } 
+  
+  
+  else {
+    return (
+      <SortableGridview
+        data={bookLogic}
+        onDragStart={() => {
+          console.log("LockItemCoverLayout onDragStart");
+        }}
+        onDragRelease={(data) => {
+          console.log("LockItemCoverLayout onDragRelease", data);
+        }}
+        renderItem={(item, index) => {
+          return (
+            <View
+              key={[item.name]}
+              style={[
+                styles.item,
+                {
+                  backgroundColor: item.backgroundColor,
+                  width: "100px",
+                  height: "130px",
+                  resizeMode: "cover",
+                  backgroundImage: item.backgroundImage,
+                },
+              ]}
+              onTap={() => {
+                Alert.alert(`On Tap ${item.name}!`);
+              }}
+            >
+              <Text
+                style={[styles.text, { color: item.color }]}
+                onPress={() => {
+                  setFlag(!flag);
+                }}
+              >
+                {item.name}
+              </Text>
+            </View>
+          );
+        }}
+      />
+    );
+  }
+}
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 15,
+    backgroundColor: "#ddd",
+  },
+  item: {
+    width: 80,
+    borderRadius: 12,
+    height: 70,
+    color: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: "16%",
+    marginTop: "20%",
+  },
+  top: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: "345%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    marginLeft: "-4%",
+  },
+  lock: {
+    width: 80,
+    margin: 8,
+    borderRadius: 12,
+    height: 70,
+    backgroundColor: "gray",
+    color: "#fff",
+    textAlign: "center",
+    justifyContent: "center",
+  },
+});
