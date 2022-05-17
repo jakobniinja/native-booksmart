277aae894f898c01629f81a7552e02b6b9360595
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-05-17 19:53:22 +0200

    refactored out library logic

---
+++ assets/data.js
@@ -0,0 +1,74 @@
+import l1 from "./books/lm1-rs.jpg";
+import l2 from "./books/lm2-rs.jpg";
+
+import da1 from "./books/dfmf-rs.jpg";
+import da2 from "./books/dfmf2-rs.jpg";
+import da3 from "./books/dfmf3-rs.jpg";
+import hp from "./books/hp-full (1).jpg";
+import h1 from "./books/filosofi-stenen-rs.jpg";
+import j1 from "./books/spionb.jpg";
+import h2 from "./books/halvblodsprinsen-rs.jpg";
+import h3 from "./books/fenfix-rs.jpg";
+
+
+
+
+export let data2 = [
+  {
+    name: "På Ny Kula",
+    backgroundColor: "#09f",
+    color: "#000",
+
+    backgroundImage: `url(${da3})`,
+  },
+  {
+    name: "Bravader ",
+    color: "#000",
+    backgroundImage: `url(${da1})`,
+  },
+  {
+    name: "Nya Fältet",
+    backgroundColor: "#rgba(0, 222, 144, 1)",
+    color: "#000",
+    backgroundImage: `url(${da2})`,
+  },
+];
+export let data3 = [
+  {
+    name: "Hotell Mysteriet ",
+    color: "#000",
+    backgroundImage: `url(${l1})`,
+  },
+  {
+    name: "Diamant Myseriet",
+    backgroundColor: "#rgba(0, 222, 144, 1)",
+    color: "#000",
+    backgroundImage: `url(${l2})`,
+  },
+  {
+    name: "jakobs book",
+    backgroundColor: "#rgba(0, 222, 144, 1)",
+    color: "#000",
+    backgroundImage: `url(${j1})`,
+  },
+];
+
+export let data4 = [
+  {
+    name: "Halvblodsprinsen",
+    color: "#000",
+    backgroundImage: `url(${h1}`,
+  },
+  {
+    name: "Filosofi stenen",
+    backgroundColor: "#rgba(0, 222, 144, 1)",
+    color: "#000",
+    backgroundImage: `url(${h2})`,
+  },
+  {
+    name: "Fenixordern",
+    backgroundColor: "#rgba(0, 222, 144, 1)",
+    color: "#000",
+    backgroundImage: `url(${h3})`,
+  },
+];
--- components/BookGridHelper.js
+++ components/BookGridHelper.js
@@ -2,38 +2,10 @@ import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
 import SortableGridview from "react-native-sortable-gridview";
 import React, { useContext, useEffect, useState } from "react";
 import AppContext from "../Context/AppContext";
-import l1 from "../assets/books/lm1-rs.jpg";
-import l2 from "../assets/books/lm2-rs.jpg";
-
-import da1 from "../assets/books/dfmf-rs.jpg";
-import da2 from "../assets/books/dfmf2-rs.jpg";
-import da3 from "../assets/books/dfmf3-rs.jpg";
-import hp from "../assets/books/hp-full (1).jpg";
-import h1 from "../assets/books/filosofi-stenen-rs.jpg";
-import j1 from "../assets/books/spionb.jpg";
-import h2 from "../assets/books/halvblodsprinsen-rs.jpg";
-import h3 from "../assets/books/fenfix-rs.jpg";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {data2, data3, data4} from "../assets/data"
 
 export default function BookGridHelper() {
   const { user, setUser, setClicked, clicked } = useContext(AppContext);
-  const [bg, setBg] = useState({
-    name: "Bravader ",
-    color: "#000",
-    backgroundImage: `url(${da1})`,
-  });
-
-
-
-
-  if(user.name =="Lilla Captain"){
-
-
-
-  setTimeout(function() {
-    location.reload();
-  }, 25000);
-  }
 
 
   const [bookLogic, setBookLogic] = useState([
@@ -45,65 +17,7 @@ export default function BookGridHelper() {
     },
   ]);
 
-  let data2 = [
-    {
-      name: "På Ny Kula",
-      backgroundColor: "#09f",
-      color: "#000",
 
-      backgroundImage: `url(${da3})`,
-    },
-    {
-      name: "Bravader ",
-      color: "#000",
-      backgroundImage: `url(${da1})`,
-    },
-    {
-      name: "Nya Fältet",
-      backgroundColor: "#rgba(0, 222, 144, 1)",
-      color: "#000",
-      backgroundImage: `url(${da2})`,
-    },
-  ];
-  let data3 = [
-    {
-      name: "Hotell Mysteriet ",
-      color: "#000",
-      backgroundImage: `url(${l1})`,
-    },
-    {
-      name: "Diamant Myseriet",
-      backgroundColor: "#rgba(0, 222, 144, 1)",
-      color: "#000",
-      backgroundImage: `url(${l2})`,
-    },
-    {
-      name: "jakobs book",
-      backgroundColor: "#rgba(0, 222, 144, 1)",
-      color: "#000",
-      backgroundImage: `url(${j1})`,
-    },
-  ];
-
-  let data4 = [
-    {
-      name: "Halvblodsprinsen",
-      color: "#000",
-      backgroundImage: `url(${h1}`,
-    },
-    {
-      name: "Filosofi stenen",
-      backgroundColor: "#rgba(0, 222, 144, 1)",
-      color: "#000",
-      backgroundImage: `url(${h2})`,
-    },
-    {
-      name: "Fenixordern",
-      backgroundColor: "#rgba(0, 222, 144, 1)",
-      color: "#000",
-      backgroundImage: `url(${h3})`,
-    },
-  ];
   if (user.name == "Tofflan") {
     return (
       <SortableGridview
